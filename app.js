f2c2008a717d4eb03b60be0fd4b34eadcb4e8ce6
Anurag <anuragom2002@gmail.com>
2026-02-01 23:01:47 +0530

    Initial commit: basic Windows 7 style boot screen

---
+++ app.js
@@ -0,0 +1,10 @@
+const text = document.querySelector(".boot-text");
+setTimeout(() => {
+  text.style.opacity = "1";
+}, 1000);
+
+const logo = document.querySelector(".win-logo img");
+
+setTimeout(() => {
+  logo.style.opacity = "1";
+}, 1000);

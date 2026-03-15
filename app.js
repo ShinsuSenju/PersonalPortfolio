4c785ae5e512122ba087504fad4d9477345a1371
Anurag <anuragom2002@gmail.com>
2026-03-16 01:51:18 +0530

    Added login screen

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const text = document.querySelector(".boot-text");
-setTimeout(() => {
-  text.style.opacity = "1";
-}, 1000);
-
+const bootScreen = document.getElementById("boot");
+const loginScreen = document.getElementById("login-screen");
 const logo = document.querySelector(".win-logo img");
+const bootText = document.querySelector(".boot-text");
 
-setTimeout(() => {
-  logo.style.opacity = "1";
-}, 1000);
+window.onload = () => {
+  setTimeout(() => {
+    logo.style.opacity = "1";
+    bootText.style.opacity = "1";
+  }, 500);
+
+  setTimeout(() => {
+    bootScreen.classList.add("hidden");
+    loginScreen.classList.remove("hidden");
+  }, 4000);
+};

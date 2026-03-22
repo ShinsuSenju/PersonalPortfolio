c7e3f9e9705c1ebf3cd749c958dc41a8e812e010
Anurag <anuragom2002@gmail.com>
2026-03-22 14:26:08 +0530

    Added authentication and transition to desktop with sound

--- app.js
+++ app.js
@@ -14,3 +14,71 @@ window.onload = () => {
     loginScreen.classList.remove("hidden");
   }, 4000);
 };
+
+const loginBtn = document.querySelector(".blue-arrow-btn");
+const passwordInput = document.querySelector(".login-input");
+const hintDisplay = document.getElementById("password-hint");
+const wikiCard = document.getElementById("wiki-card");
+const desktop = document.getElementById("desktop");
+
+// Old: const SECRET_PASSWORD = "linus";
+
+const VALID_PASSWORDS = ["linus", "linus torvalds", "torvalds"];
+
+let failedAttempts = 0;
+let isTauntActive = false;
+
+function attemptLogin() {
+  const userGuess = passwordInput.value.toLowerCase().trim();
+
+  if (VALID_PASSWORDS.includes(userGuess.toLowerCase())) {
+    const audio = new Audio("startup.mp3");
+    audio.play().catch((e) => console.log("Audio Blocked"));
+    loginScreen.classList.add("hidden");
+    desktop.classList.remove("hidden");
+    return;
+  }
+
+  if (isTauntActive) {
+    passwordInput.value = "linus";
+
+    hintDisplay.innerText = "There you go. Now hit the arrow to boot up.";
+    hintDisplay.style.color = "#88daff";
+
+    wikiCard.classList.remove("hidden");
+    setTimeout(() => wikiCard.classList.add("show"), 10);
+
+    isTauntActive = false;
+    return;
+  }
+
+  failedAttempts++;
+  passwordInput.value = "";
+  passwordInput.focus();
+
+  passwordInput.classList.add("input-error");
+  setTimeout(() => {
+    passwordInput.classList.remove("input-error");
+  }, 400);
+
+  hintDisplay.classList.remove("hidden");
+  setTimeout(() => hintDisplay.classList.add("show"), 10);
+
+  if (failedAttempts === 1) {
+    hintDisplay.innerText =
+      "Hint: The creator of a much better, open-source OS (kernel actually!)...";
+    hintDisplay.style.color = "white";
+  } else if (failedAttempts >= 2) {
+    hintDisplay.innerText =
+      "Typical Windows user... Just switch to Linux already. Press Enter to admit defeat and get the password.";
+    hintDisplay.style.color = "#ffffff";
+    isTauntActive = true;
+  }
+}
+
+loginBtn.addEventListener("click", attemptLogin);
+passwordInput.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    attemptLogin();
+  }
+});
